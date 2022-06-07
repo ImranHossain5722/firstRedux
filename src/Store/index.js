aef30a35ca61fd129f561d2e7ddd7b8416b66d20
ImranHossain5722 <imd572258@gmail.com>
2022-06-07 16:58:31 +0600

    practice redux

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,7 +1,65 @@
 import { createStore } from "redux";
 import rootReducers from "./reducers/rootReducers";
 
-const store =createStore (rootReducers)
+// const store =createStore (rootReducers)
+
+
+const INCREMENT = 'Increment';
+const DECREMENT = 'Decrement';
+
+// state
+const initialState = {
+    count:0
+}
+
+// action
+const incrementAction =() =>{
+
+    return{
+
+        type: INCREMENT,
+    }
+}
+const decrementAction =() =>{
+
+    return{
+
+        type: DECREMENT,
+    }
+}
+
+
+//reducer
+const counterReducer = (state= initialState,action)=>{
+
+switch (action.type) {
+    case INCREMENT:
+        return{
+            ...state,
+            count:state.count +1
+        }
+    case DECREMENT:
+        return{
+            ...state,
+            count:state.count -1
+        }
+
+    default:
+        break;
+}
+}
+
+
+//Store 
+const store = createStore(counterReducer)
+
+store.subscribe(()=>{
+
+    console.log(store.getState())
+})
+
+store.dispatch (incrementAction())
+
 
 export default store
 
